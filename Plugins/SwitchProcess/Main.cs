46c6a19be574c68c2885b8d1be2addbb9d618716
Binlong Li <bil@microsoft.com>
2016-11-06 23:31:58 -0800

    Create Switch Process Plugin project, and tested working on Wox.

---
+++ Plugins/SwitchProcess/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Plugin;
+
+namespace SwitchProcess
+{
+    public class Main : IPlugin
+    {
+        public List<Result> Query(Query query)
+        {
+            var result = new Result
+            {
+                Title = "Switch Process",
+                SubTitle = $"Query: {query.Search}",
+                IcoPath = "app.png"
+            };
+            return new List<Result> { result };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+
+        }
+    }
+}
